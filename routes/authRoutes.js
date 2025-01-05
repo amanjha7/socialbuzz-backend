bfd41542fef9e2d15406743d32f052cfbb31da06
Aman Jha <amanj@pronnel.com>
2025-01-05 16:19:42 +0530

    added middleware to know who wanna change password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/authcontroller');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/change-password', authController.changePassword);
+router.put('/change-password', authMiddleware, authController.changePassword);
 
 module.exports = router;
